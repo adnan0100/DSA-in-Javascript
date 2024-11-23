054e9b2b3ca3775801d5467ca86d268ccf659313
Adnan <adnanrocker100@gmail.com>
2024-11-23 13:46:32 +0500

    New Commit

---
+++ Basics/ArrayMethods.js
@@ -0,0 +1,25 @@
+// Length of array
+
+// let arr=[23,12,55,67,89,10]
+// console.log(arr.length)
+
+// Adding element to end of array
+
+// let arr1=[30,6,23,190,54,1]
+// arr1.push(32)
+// console.log(arr1)
+
+// // Removing last element from array
+// let arr2=[23,56,44,67,80,12]
+// arr2.pop()
+// console.log(arr2)
+
+// finding index of specific element
+
+// let arr=[12,23,44,66,89,17,31]
+// console.log(arr.indexOf(66))
+
+// converting array into string
+let arr2=[23,45,33,10,78,59]
+let result= arr2.join(" , ")
+console.log(result)
